cf248bbb29d9c65d52e20726f80c800b4d61b752
Erick Chacon <eral.th07@gmail.com>
2022-03-14 20:32:31 -0500

    patches sh

--- patches.sh
+++ patches.sh
@@ -3,13 +3,5 @@
 git checkout base
 git branch -D patched
 git checkout -b patched
-# git merge dwmlogo -m "dwmlogo"
-git merge message -m "message"
+git merge dwmlogo -m "dwmlogo"
 git merge -Xtheirs config -m "config"
-# git merge autostart -m "autostart patch"
-# git merge attachbelow -m "attachbelow"
-# git merge vanitygapss -m "vanitygaps: custom centered master"
-# git merge fakefullscreentoggle -m "fakefullscreen"
-# git merge movestack -m "movestach patch"
-# git merge titlecolor -m "titlecolor"
-# git merge fixborders -m "fixborders"
